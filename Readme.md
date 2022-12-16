b7244e2ca9a9177232e53d214e55c3d8112c244d
Aly <alcamara237@gmail.com>
2022-12-16 05:50:09 -0500

    Finished the landing page content.

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-Stopped at 2:02
+Start at 2:02
+
+Note: Finish adding the animation when you reload the page for the landing section
---
+++ index.js
@@ -0,0 +1,4 @@
+function FormClicked() {
+    let InputForm = document.body.getElementsByClassName("about__input--form")[0];
+    InputForm.classList += " clicked";
+}
